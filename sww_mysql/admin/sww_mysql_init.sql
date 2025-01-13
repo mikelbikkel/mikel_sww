a0203e550cafd8e743090afdff45cd529a5aacae
mikel bikkel <mikel@gaweg.com>
2025-01-13 17:11:44 +0100

    update for mysql 8

--- sww_mysql/admin/sww_mysql_init.sql
+++ sww_mysql/admin/sww_mysql_init.sql
@@ -7,19 +7,24 @@
 --
 -- mysql -u root -t -vvv -p < scriptname.sql > scriptname_log.txt
 --
--- TODO: schema, owner user, access users (r, rw, .....)
--- Version Date     Who  Remark
--- 1.0     12-12-09 MVD  Initial version
---
+-- Version Date       Who  Remark
+-- 1.0     12-12-09   MVD  Initial version
+-- 1.1     2025-01-13 MVD  Update for mysql version 8
+--                         charset utf8 obsolete. Change to:
+--                         - utf8mb4 [supports BMP and supplementary characters].
+--                         - collate utf8mb4_0900_ai_ci
+--                           0900 refers to the Unicode Collation Algorithm version.
+--                           ai refers accent insensitivity when sorting. 
+--                           ci refers to case insensitivity when sorting.
 
-CREATE SCHEMA IF NOT EXISTS `sww2` 
-  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE SCHEMA IF NOT EXISTS sww2 
+  DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 SHOW WARNINGS;
 
 -- The @'localhost' means that sww_owner can only connect from localhost.
 -- Use @'%'to allow sww_owner to connect from any host.
-CREATE USER 'sww_owner'@'localhost' IDENTIFIED BY 'XXX';
+CREATE USER 'sww_owner'@'%' IDENTIFIED WITH caching_sha2_password BY 'XXX';
 
 SHOW WARNINGS;
 
@@ -30,20 +35,21 @@ GRANT
 , DROP
 , CREATE, CREATE TEMPORARY TABLES, CREATE VIEW, INDEX, CREATE ROUTINE
 , ALTER, ALTER ROUTINE, SHOW VIEW
+, REFERENCES
   ON sww2.*
-  TO 'sww_owner'@'localhost'
+  TO 'sww_owner'@'%'
 ;
 
 SHOW WARNINGS;
 
-CREATE USER 'sww_reader'@'localhost' IDENTIFIED BY 'sww_reader';
+CREATE USER 'sww_reader'@'%' IDENTIFIED WITH caching_sha2_password BY 'sww_reader';
 
 SHOW WARNINGS;
 
 GRANT 
   SELECT
   ON sww2.*
-  TO 'sww_reader'@'localhost'
+  TO 'sww_reader'@'%'
 ;
 
 SHOW WARNINGS;
